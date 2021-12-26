b9f8c746df2608e3fa4544f7160af9db5aab528f
Artiom-Evs <evs.a.s@mail.ru>
2021-12-26 16:24:09 +0300

    Добавьте файлы проекта.

---
+++ NFTelegram/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NFTelegram
+{
+    public class NFTelegram
+    {
+        // Browse our samples repository: https://github.com/nanoframework/samples
+        // Check our documentation online: https://docs.nanoframework.net/
+        // Join our lively Discord community: https://discord.gg/gCyBu8T
+    }
+}
